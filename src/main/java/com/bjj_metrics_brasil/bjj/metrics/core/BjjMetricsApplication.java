cadee4e42e5b289bb6d334809aae5e7d81b5d434
Mateus-Cristofori <ajhororow75@gmail.com>
2026-04-01 19:54:15 -0500

    Feat: iniciando o projeto

---
+++ src/main/java/com/bjj_metrics_brasil/bjj/metrics/core/BjjMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.bjj_metrics_brasil.bjj.metrics.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjjMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjjMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjj_metrics_brasil/bjj/metrics/BjjMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjj_metrics_brasil.bjj.metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjjMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
