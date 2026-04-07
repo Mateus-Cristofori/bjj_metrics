583d07af5ad624f2f6f7e10216323128211e2835
Mateus-Cristofori <ajhororow75@gmail.com>
2026-04-07 00:42:58 -0500

    Feat: resgatando o valor do userId atrÃavés do token de autenticação

--- authentication/src/main/java/com/bjj_metrics_brasil/authentication/config/token/service/TokenService.java
+++ authentication/src/main/java/com/bjj_metrics_brasil/authentication/config/token/service/TokenService.java
@@ -1,8 +1,10 @@
 package com.bjj_metrics_brasil.authentication.config.token.service;
 
 import com.bjj_metrics_brasil.authentication.repository.entity.Users;
+import java.util.UUID;
 
 public interface TokenService {
     String generateToken(Users user);
     String generateRefreshToken(Users user);
+    UUID getUserIdFromToken(String token);
 }
---
+++ common/src/main/java/com/bjj_metrics_brasil/annotation/resolver/CurrentUserIdResolver.java
@@ -0,0 +1,41 @@
+package com.bjj_metrics_brasil.annotation.resolver;
+
+import com.bjj_metrics_brasil.annotation.CurrentUserId;
+import java.util.UUID;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class CurrentUserIdResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUserId.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+        @NonNull MethodParameter parameter,
+        ModelAndViewContainer mavContainer,
+        @NonNull NativeWebRequest webRequest,
+        WebDataBinderFactory binderFactory
+    ) {
+        Authentication authentication = SecurityContextHolder
+            .getContext()
+            .getAuthentication();
+
+        if (
+            authentication != null && authentication.getPrincipal() instanceof UUID userId
+        ) {
+            return userId;
+        }
+        return null;
+    }
+}
